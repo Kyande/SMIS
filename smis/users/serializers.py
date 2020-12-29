cc899c093341b5d623c30f57ae38c5cd5620e41f
Kyande20 <kyande20@gmail.com>
2020-12-29 14:31:49 +0300

    change serializer to HyperlinkedModel serializer

--- smis/users/serializers.py
+++ smis/users/serializers.py
@@ -3,13 +3,13 @@ from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     date_joined = serializers.DateTimeField(read_only=True)
     last_login = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = User
         fields = [
-            'avatar', 'email', 'first_name', 'last_name',
-            'date_joined', 'last_login', 'user_type',
-            'is_active']
+            'url', 'id', 'avatar', 'email',
+            'first_name', 'last_name', 'date_joined',
+            'last_login', 'user_type', 'is_active']
