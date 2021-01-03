b7baaaab5f1e0f146f21922ccb8d4a3aa17c79a0
Kyande20 <kyande20@gmail.com>
2021-01-03 16:10:45 +0300

    Register User model in admin

--- smis/users/admin.py
+++ smis/users/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+def reactivate_users(modeladmin, request, queryset):
+    queryset.update(is_active=True)
+
+
+reactivate_users.short_description = "Activate selected users"
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    actions = [reactivate_users]
+    date_hierarchy = 'date_joined'
+    empty_value_display = '-empty-'
+    exclude = ('date_joined', 'is_active', )
+    fields = (
+        'avatar',
+        ('first_name', 'last_name'),
+        'email',
+        'user_type',
+        'is_superuser',
+        'is_staff',
+    )
+    list_display = (
+        'email', 'first_name', 'last_name',
+        'date_joined', 'user_type', 'is_active',
+        'is_staff')
+    list_display_links = ('email', )
+    list_filter = ('user_type', 'is_active', 'is_staff', )
+    list_per_page = 50
+    ordering = ('first_name', 'last_name', )
+    search_fields = ('first_name', 'last_name', 'email')
