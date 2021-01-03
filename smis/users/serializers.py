bf4e228c51f3795bcbe97c4333b77bead395bcaf
Kyande20 <kyande20@gmail.com>
2021-01-03 15:22:34 +0300

    Add password change and user deactivation serialzer

--- smis/users/serializers.py
+++ smis/users/serializers.py
@@ -6,6 +6,7 @@ from .models import User, USER_TYPES
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     date_joined = serializers.DateTimeField(read_only=True)
     last_login = serializers.DateTimeField(read_only=True)
+    is_active = serializers.BooleanField(read_only=True)
 
     class Meta:
         model = User
@@ -62,5 +63,34 @@ class UserRegistrationSerializer(serializers.Serializer):
         attrs = super().validate(attrs)
         if attrs['password'] != attrs['repeat_password']:
             raise serializers.ValidationError(
-                {"password": "your passwords do not match"})
+                {"password": "Passwords do not match"})
         return attrs
+
+
+class PasswordChangeSerializer(serializers.Serializer):
+    old_password = serializers.CharField(
+        min_length=8, max_length=10, write_only=True,
+        allow_null=False, required=True, trim_whitespace=True)
+    new_password = serializers.CharField(
+        min_length=8, max_length=10, write_only=True,
+        allow_null=False, required=True, trim_whitespace=True)
+    repeat_new_password = serializers.CharField(
+        min_length=8, max_length=10, write_only=True,
+        allow_null=False, required=True, trim_whitespace=True)
+
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+        if attrs['old_password'] == attrs['new_password']:
+            raise serializers.ValidationError(
+                {"password": "New password should not match old password"})
+
+        if attrs['new_password'] != attrs['repeat_new_password']:
+            raise serializers.ValidationError(
+                {"password": "New passwords do not match"})
+        return attrs
+
+
+class UserDeactivationSerializer(serializers.Serializer):
+    password = serializers.CharField(
+        min_length=8, max_length=10, write_only=True,
+        allow_null=False, required=True, trim_whitespace=True)
